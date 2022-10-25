482f99acef31bb285101bb2159cbb7e77333b19b
erencanucarr <116668477+erencanucarr@users.noreply.github.com>
2022-10-25 19:54:15 +0300

    Create introduce_yourself.c

---
+++ introduce_yourself.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+	
+	printf("Hi\n");
+	getch();
+	printf("I'am Can!\n");
+	getch();
+	printf("Welcome To My Profile!\n");
+	getch();
+	printf("See YOu Later.\n");
+	getch();
+}
